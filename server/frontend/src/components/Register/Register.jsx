76f52bb487f559d8bc3740b1bd7fa6211143fe3b
MuturiIsaac <mbuguaisaac68@gmail.com>
2024-05-06 00:24:29 +0300

    registration ui updates

--- server/frontend/src/components/Register/Register.jsx
+++ server/frontend/src/components/Register/Register.jsx
@@ -46,7 +46,28 @@ const Register = () => {
   return (
     <div>
       <Header />
-      <div onClick={(e) => { e.stopPropagation(); }} className='modalContainer'>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <a className="navbar-brand" href="#">Logo</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="modalContainer">
         <div className="banner py-2">
           <h1 className="mb-0">Register</h1>
         </div>
