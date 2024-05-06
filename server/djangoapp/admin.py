c10dfad8be705a55b71af0b785488eef63175174
MuturiIsaac <mbuguaisaac68@gmail.com>
2024-05-06 11:47:01 -0400

    models and views  update populate addition

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,12 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
+@admin.register(CarMake)
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
 
-# Register your models here.
-
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+@admin.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')
+    list_filter = ('type', 'car_make', 'year')
+    search_fields = ('name', 'car_make__name')
